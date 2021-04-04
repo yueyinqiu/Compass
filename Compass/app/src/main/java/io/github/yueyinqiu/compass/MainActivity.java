c5f39f748b280eba9d319650c938ef1d9fd3d46a
yueyinqiu <lujiaqi2003@126.com>
2021-04-05 00:38:53 +0800

    first lines

---
+++ Compass/app/src/main/java/io/github/yueyinqiu/compass/MainActivity.java
@@ -0,0 +1,167 @@
+package io.github.yueyinqiu.compass;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.lqr.picselect.LQRPhotoSelectUtils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Compass compass;
+    private ImageView arrowView;
+    private float currentAzimuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        arrowView = findViewById(R.id.imageView);
+        setupCompass();
+        try(FileInputStream inputStream = openFileInput("imagePath"))
+        {
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String line = bufferedReader.readLine();
+            Uri uri = Uri.parse(line);
+            arrowView.setImageURI(uri);
+        }
+        catch (Exception ignore)
+        {
+        }
+
+        mLqrPhotoSelectUtils = new LQRPhotoSelectUtils(
+                this, new LQRPhotoSelectUtils.PhotoSelectListener() {
+            @Override
+            public void onFinish(File outputFile, Uri outputUri) {
+                arrowView.setImageURI(outputUri);
+                try(FileOutputStream outputStream = openFileOutput("imagePath", Context.MODE_PRIVATE))
+                {
+                    OutputStreamWriter writer = new OutputStreamWriter(outputStream);
+                    BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                    bufferedWriter.write(outputUri.toString());
+                    bufferedWriter.flush();
+                }
+                catch (IOException ignore)
+                {
+                }
+            }
+        }, false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // 菜单的监听方法
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.view_repo:
+                Uri uri = Uri.parse("https://github.com/yueyinqiu/Compass");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                break;
+            case R.id.select_image:
+                if( ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]
+                                {
+                                        Manifest.permission.READ_EXTERNAL_STORAGE
+                                }
+                        , 233);
+                mLqrPhotoSelectUtils.selectPhoto();
+                break;
+        }
+        return true;
+    }
+    private void setupCompass() {
+        compass = new Compass(this);
+        Compass.CompassListener cl = getCompassListener();
+        compass.setListener(cl);
+    }
+
+    private void adjustArrow(float azimuth) {
+        Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        currentAzimuth = azimuth;
+
+        an.setDuration(500);
+        an.setRepeatCount(0);
+        an.setFillAfter(true);
+
+        arrowView.startAnimation(an);
+    }
+
+    LQRPhotoSelectUtils mLqrPhotoSelectUtils;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mLqrPhotoSelectUtils.attachToActivityForResult(requestCode, resultCode, data);
+    }
+
+    private Compass.CompassListener getCompassListener() {
+        return new Compass.CompassListener() {
+            @Override
+            public void onNewAzimuth(final float azimuth) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adjustArrow(azimuth);
+                    }
+                });
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        compass.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        compass.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        compass.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        compass.stop();
+    }
+
+}
